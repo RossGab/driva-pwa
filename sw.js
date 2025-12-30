a862bdf70fa86c6f2185875412427f0039fc509b
RossGab <rcnitura@gmail.com>
2025-12-31 05:18:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
+const CACHE_NAME = "driver-app-v1";
+
+const APP_SHELL = [
+  "./",
+  "./driver.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js",
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js"
+];
+
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
   );
 });
