e47fc048fe9b9f2cb5a6c852110fe923308f282d
RossGab <rcnitura@gmail.com>
2026-01-16 03:02:32 +0800

    Create report.js

---
+++ report.js
@@ -0,0 +1,6 @@
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase, ref, get
+} from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
